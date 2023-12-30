132c9e447311b79436c7fb2ee8b721eb93831c1f
nate hishon <nathan.hishon@gmail.com>
2023-12-30 11:56:39 -0500

    test commit

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-  echo "hey"
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CISC 3300 SP24</title>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    </head>
+    <body>
+
+        class!
+
+    </body>
+</html>
