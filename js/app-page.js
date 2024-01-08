4fe08535657f434413148643a331c72edb09d030
nate hishon <nathan.hishon@gmail.com>
2024-01-08 16:48:41 -0500

    nav updates

--- js/app-page.js
+++ js/app-page.js
@@ -2,35 +2,42 @@
  * Dropdown menu
  */
 
-document.addEventListener('DOMContentLoaded', function() {
-  const menus = document.querySelectorAll('.navbar-burger');
-  const dropdowns = document.querySelectorAll('.navbar-menu');
-  
-  if (menus.length && dropdowns.length) {
-    for (var i = 0; i < menus.length; i++) {
-      menus[i].addEventListener('click', function() {
-        for (var j = 0; j < dropdowns.length; j++) {
-          dropdowns[j].classList.toggle('is-active');
-        }
-      });
-    }
-  }
-});
 
-console.log('yo')
-console.log(location.pathname)
+
 
 $( document ).ready(function() {
-  console.log( "ready!" );
   
-  if (location.pathname === '/') {
-    console.log('selected')
-    $( "#homepage-navbar-item" ).addClass( "is-active" );
-  }
+  console.log(location.pathname)
+  $("#navContent").load("header.html", function () {
+    if (location.pathname === '/') {
+      $( "#homepage-navbar-item" ).addClass( "is-active" );
+    }
   
-  if (location.pathname === '/schedule.html') {
-    console.log('selected2')
-    $( "#schedule-navbar" ).addClass( "is-active" );
-  }
+    if (location.pathname === '/schedule.html') {
+      $( "#schedule-navbar" ).addClass( "is-active" );
+    }
+    if (location.pathname === '/resources.html') {
+      $( "#resources-navbar" ).addClass( "is-active" );
+    }
+    if (location.pathname === '/other.html') {
+      $( "#other-navbar" ).addClass( "is-active" );
+    }
+  
+    const menus = document.querySelectorAll('.navbar-burger');
+    const dropdowns = document.querySelectorAll('.navbar-menu');
+  
+    if (menus.length && dropdowns.length) {
+      for (var i = 0; i < menus.length; i++) {
+        menus[i].addEventListener('click', function() {
+          for (var j = 0; j < dropdowns.length; j++) {
+            dropdowns[j].classList.toggle('is-active');
+          }
+        });
+      }
+    }
+    
+  });
+  
+  
   
 });
